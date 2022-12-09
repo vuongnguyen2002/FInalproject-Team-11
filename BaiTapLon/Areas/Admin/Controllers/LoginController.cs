c4dd6e257f4167cb0eeac60a546e35d3d717bae9
unknown <you@example.com>
2022-12-10 04:48:14 +0700

    kfkf

---
+++ BaiTapLon/Areas/Admin/Controllers/LoginController.cs
@@ -0,0 +1,66 @@
+﻿using BaiTapLon.Areas.Admin.Models;
+using BaiTapLon.Common;
+using Mood.Draw;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace BaiTapLon.Areas.Admin.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Admin/Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Login(LoginModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var draw = new UserDraw();
+                var result = draw.Login(model.userName,EncryptorMD5.GetMD5(model.passWord));
+                if (result == 1)
+                {
+                    var user = draw.getByID(model.userName);
+                    var userSession = new AdminLogin();
+                    userSession.userName = user.UserName;
+                    userSession.userId = user.IDUser;
+                    Session.Add(Constant.ADMIN_SESSION, userSession);
+                    return RedirectToAction("Index", "Homes");
+                }
+                else if(result == -3)
+                {
+                    ModelState.AddModelError("", "This account has no access rights!!!!!!");
+                }
+                else if(result == -1)
+                {
+                    ModelState.AddModelError("", "Account has been locked !!!");
+                }else if(result == -2)
+                {
+                    ModelState.AddModelError("", "wrong password!!!");
+                }
+                else if(result == 0)
+                {
+                    ModelState.AddModelError("", "Account does not exist!!!");
+                }
+                else{
+                    ModelState.AddModelError("", "Login failed!!!");
+                }
+            }
+            else
+            {
+                ModelState.AddModelError("", "Login failed!!!");
+            }
+            return View("Index");
+        }
+        public ActionResult Logout()
+        {
+            Session[Constant.ADMIN_SESSION] = null;
+            return View("Index");
+        }
+    }
+}
---
+++ BaiTapLon/Areas/Admin/Controllers/NhaCungCapController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Mood.Draw;
+using Mood.EF2;
+
+namespace BaiTapLon.Areas.Admin.Controllers
+{
+    public class NhaCungCapController : BaseController
+    {
+        // GET: Admin/NhaCungCap
+        public ActionResult Index(string searchString, int page = 1, int pagesize = 5)
+        {
+            var draw = new NhaCungCapDraw();
+            var model = draw.listALL(searchString,page,pagesize);
+            return View(model);
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            SetViewBangIDUser();
+            return View();
+            
+        }
+        [HttpPost]
+        public ActionResult Create(NhaCungCap model)
+        {
+            if(ModelState.IsValid)
+            {
+                var nhaCungCap = new NhaCungCapDraw();
+                model.NgayTao = DateTime.Now;
+                if(!nhaCungCap.CheckNCC(model.TenNCC))
+                {
+                    if(nhaCungCap.CheckSDT(model.SoDienThoai))
+                    {
+                        if(nhaCungCap.IsValidEmail(model.Email))
+                        {
+                            var result = nhaCungCap.Insert(model);
+                            if (result > 0)
+                            {
+                                ModelState.AddModelError("nhacungcapSuccess", "Add success");
+                            }
+                            else
+                            {
+                                ModelState.AddModelError("nhacungcap", "Add failure");
+                            }
+                        }else
+                        {
+                            ModelState.AddModelError("nhacungcap", " Email invalidate!!!");
+                        }
+                    }else
+                    {
+                        ModelState.AddModelError("nhacungcap", " Invalid phone number!!!");
+                    }
+                }else
+                {
+                    ModelState.AddModelError("nhacungcap", " Vendor name already exists!!!");
+                }
+            }else
+            {
+                ModelState.AddModelError("nhacungcap", " Add invalid data!!!");
+            }
+            SetViewBangIDUser();
+            return View("Create");
+        }
+        [HttpGet]
+        public ActionResult Edit(long id)
+        {
+            SetViewBangIDUser();
+            var model = new NhaCungCapDraw().getByID(id);
+            return View(model);
+        }
+        [HttpPost]
+        public ActionResult Edit(NhaCungCap model)
+        {
+            if(ModelState.IsValid)
+            {
+                var nhaCungCap = new NhaCungCapDraw();
+
+                model.NgayTao = DateTime.Now;
+                    if (nhaCungCap.CheckSDT(model.SoDienThoai))
+                    {
+                        if (nhaCungCap.IsValidEmail(model.Email))
+                        {
+                            var result = nhaCungCap.Update(model);
+                            if (result)
+                            {
+                                ModelState.AddModelError("nhacungcapSuccess", "Add success");
+                            }
+                            else
+                            {
+                                ModelState.AddModelError("nhacungcap", "More failure");
+                            }
+                        }
+                        else
+                        {
+                            ModelState.AddModelError("nhacungcap", "Email invalidate !!!");
+                        }
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("nhacungcap", " Invalid phone number!!!");
+                    }
+              
+            }
+            else
+            {
+                ModelState.AddModelError("nhacungcap", "Update invalid data !!!");
+            }
+            SetViewBangIDUser();
+            return View("Edit");
+
+        }
+        public void SetViewBangIDUser(long? selectID = null)
+        {
+            var dao = new NhaCungCapDraw();
+            ViewBag.IDNguoiTao = new SelectList(dao.listALLUserAdmin(), "IDUSer", "UserName", selectID);// bên trong chưa 3 tham số list, giá trị ID, tên hiển thị
+        }
+        [HttpDelete]
+        public ActionResult Delete(long id)
+        {
+            new NhaCungCapDraw().Delete(id);
+            return View();
+        }
+    }
+}
